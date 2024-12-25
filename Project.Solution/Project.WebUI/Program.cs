c23d06b6b8783aaa0a97f2722f1ca443d8c0aeae
RahimliZakir <zakirer@code.edu.az>
2024-12-26 01:15:40 +0400

    ..

--- Project.Solution/Project.WebUI/Program.cs
+++ Project.Solution/Project.WebUI/Program.cs
@@ -1,5 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+IServiceCollection services = builder.Services;
+services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+IWebHostEnvironment env = builder.Environment;
+if (env.IsDevelopment())
+    app.UseDeveloperExceptionPage();
 
 app.MapGet("/", () => "Hello World!");
 
